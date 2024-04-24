3ce20c798784c5c01b7724139df8a7833876a10c
palp1ix <arthach.2022@gmail.com>
2024-04-25 00:00:23 +0300

    Переброс в VS

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using System.Data;
+
+namespace WebApplication1.Controllers;
+
+public class UsersController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    
+    public UsersController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+    
+    public IActionResult Users()
+    {
+        DataSet users = new User().GetUsers();
+        List<string> rows = new List<string>();
+        List<string> columns = new List<string>();
+        List<List<string>> table = new List<List<string>>();
+        foreach (DataTable dt in users.Tables)
+        {
+            foreach (DataColumn column in dt.Columns)
+                columns.Add(column.ColumnName);
+            foreach (DataRow row in dt.Rows)
+            {
+                var cells = row.ItemArray;
+                foreach (object? cell in cells)
+                    rows.Add(cell.ToString());
+            }
+        }
+        table.Add(columns);
+        table.Add(rows);
+        return View(table);
+    }
+}
